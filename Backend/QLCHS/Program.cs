36ba92dbcca7fa80029aff6d2b6fad36929236fb
dat <dat1211dat@gmail.com>
2023-12-04 02:51:23 +0700

    thêm vào giỏ hàng

--- Backend/QLCHS/Program.cs
+++ Backend/QLCHS/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<QLBANSACHContext>(option => option.UseSqlServer
-    (builder.Configuration.GetConnectionString("QLBANSACH_CNNET")));
+    (builder.Configuration.GetConnectionString("QLBANSACH")));
 builder.Services.AddCors(p => p.AddPolicy("MyCors", build =>
 {
     build.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
